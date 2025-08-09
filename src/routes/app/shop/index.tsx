8a10478e07d14d41ac10eccdfea5408daa76c704
Temi-pinheiro <temitopepinheiro@gmail.com>
2025-08-09 15:31:56 +0100

    all in progress pages done

--- src/routes/app/shop/index.tsx
+++ src/routes/app/shop/index.tsx
@@ -5,5 +5,14 @@ export const Route = createFileRoute('/app/shop/')({
 })
 
 function RouteComponent() {
-  return <div>Hello "/app/shop/"!</div>
+  return (
+    <div className="page">
+      <div className="w-full h-full text-center flex flex-col items-center justify-center">
+        <span className="text-4xl font-bold text-[#121212]">In Progress</span>
+        <span className="text-xl text-[#6C726F]">
+          This page is being built please stand by
+        </span>
+      </div>
+    </div>
+  )
 }
